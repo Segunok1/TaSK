324c48ac55ef0d019b4588353808b997a3f86c4f
Segunok <k077ma777@yandex.ru>
2022-08-07 10:37:06 +0300

    Решение задачи №4

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 2;
+Console.WriteLine(a);
+int b = 3;
+Console.WriteLine(b);
+int c = 7;
+Console.WriteLine(c);
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
