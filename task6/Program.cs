c68c146d5b67b6f8804d9d462e75732d5d5ea11b
Segunok <k077ma777@yandex.ru>
2022-08-07 10:41:02 +0300

    Решение задачи №6

---
+++ task6/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 4;
+Console.WriteLine(a);
+
+if(a % 2 == 0)
+{
+    Console.WriteLine("Да. Число является четным.");
+}    
+else
+{
+    Console.WriteLine("Нет. Число нечетное");
+}
+    
