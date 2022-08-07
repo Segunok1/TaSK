929839b6c040d492ffcf2db1eda999258212ac16
Segunok <k077ma777@yandex.ru>
2022-08-07 10:33:28 +0300

    Решение задачи №2

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 5;
+Console.WriteLine(a);
+int b = 7;
+Console.WriteLine(b);
+if(a > b)
+{
+   int max = a;
+   Console.Write("max = ");
+   Console.WriteLine(max);
+}
+else
+{
+    int max = b; 
+    Console.Write("max = ");
+    Console.WriteLine(max);
+} 
