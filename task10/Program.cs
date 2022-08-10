3573174cf852dee7d76f26a0b015f07648cf1f43
Segunok <k077ma777@yandex.ru>
2022-08-10 21:11:10 +0300

    Решение задачи №10

--- task10/Program.cs
+++ task10/Program.cs
@@ -2,4 +2,4 @@
 int number = Convert.ToInt32(Console.ReadLine());
 string str = Convert.ToString(number);
 Console.Write("Вторая цифра этого числа:  ");
-Console.WriteLine(str [2]);
+Console.WriteLine(str [1]);
