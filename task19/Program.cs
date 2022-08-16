51b92f18afb4e3d241f57616db0a86d554109484
Segunok1 <k077ma777@yandex.ru>
2022-08-16 21:51:28 +0300

    Решение задачи №19

--- task19/Program.cs
+++ task19/Program.cs
@@ -9,7 +9,7 @@ Console.WriteLine("Введите пятизначное число: ");
 string? number = Console.ReadLine();
 if (number!.Length == 5)
 {
-    if(number[0] == number[4] || number[1] == number[3])
+    if(number[0] == number[4] && number[1] == number[3])
     {
         Console.WriteLine("Число палиндром."); 
     }
