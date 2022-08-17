8123d6e5b54b91aa5993fc5ca0d24e74478418c4
Segunok1 <k077ma777@yandex.ru>
2022-08-17 21:52:08 +0300

    Решение задачи №25

---
+++ task25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int Exponentiation(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+         result *= a;
+    }
+    return result;
+}
+
+
+int exponentiation = Exponentiation(a, b);
+Console.WriteLine("Куб введённого чиса: "+ exponentiation);
