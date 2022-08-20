2b7d61a9e5fbc9448f7c3288f75890d1bd216688
Segunok <k077ma777@yandex.ru>
2022-08-20 23:50:17 +0300

    Решение задачи №27

---
+++ task27/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Напишите программу, которая задаёт массив из 
+8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+ Console.WriteLine("Введите числa через запятую: ");
+    
+
+void Array() 
+{
+    string[] str = Console.ReadLine().Split(",");
+    int[] array = new int[str.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(str[i].ToString());
+    }
+    for (int i = 0; i < array.Length; i++)
+    {
+    }
+    int count = str.Length;
+    int index = 0;
+    Console.Write("[");
+    while(index < count)
+    {
+        Console.Write(str[index]);
+        index++;
+        if (index < count)
+        {
+        Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}  
+
+
+Array();
