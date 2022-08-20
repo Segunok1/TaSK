5c6ae43f470b88fcfb23f0dc9bde36918e8d33c9
Segunok <k077ma777@yandex.ru>
2022-08-20 19:48:06 +0300

    Решение задачи №29

---
+++ task29/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу, которая принимает на вход число
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void SumDigits()
+{
+    string str = Convert.ToString(number);
+    int sum = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        int num = Convert.ToInt32(str[i].ToString());
+        sum += num;
+    }
+    Console.WriteLine("Сумма чисел в числе: " + sum);
+}
+
+ 
+SumDigits();
