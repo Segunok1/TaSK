5dcebe23481f308054366bb35e35107918671581
Segunok1 <k077ma777@yandex.ru>
2022-08-26 00:13:00 +0300

    Решение задачи №41

---
+++ task41/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+
+*/
+
+void NumOfPositiveNum()
+{
+    int k = 0;
+    string[] str = Console.ReadLine().Split(",");
+    int[] array = new int[str.Length];
+    for (int i = 0; i < array.Length; i++) array[i] = Convert.ToInt32(str[i].ToString());
+    for (int i = 0; i < array.Length; i++) if (array[i] > 0)  k++;
+        Console.WriteLine("Количество чётных чисел в массиве: " + k);
+}
+
+
+Console.WriteLine("Введите  M чисел с клавиатуры через запятую: ");
+NumOfPositiveNum();
