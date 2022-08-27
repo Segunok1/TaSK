76e60a9c706848ee79ff37934268952dc565b763
Segunok <k077ma777@yandex.ru>
2022-08-27 22:17:05 +0300

    Решение задачи №43.1

---
+++ task43.1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задайте двумерный массив со случайными числами от -10 до 10.
+Найдите сумму элементов, находящихся на главной диагонали
+(с индексами (0,0); (1;1) и т.д.)
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12
+*/
+
+void SumElementsMainDiagonal()
+{
+    int sum = 0;
+    int[,] array = new int[3, 4];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 10);
+            Console.Write(array[i, j] + " ");
+            if (i == j)
+            {
+                sum += array[i, j];
+            }
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine($"Сумма элементов главной диагонали: {array[0, 0]} + {array[1, 1]} + {array[2, 2]} = {sum}");
+}
+
+
+SumElementsMainDiagonal();
