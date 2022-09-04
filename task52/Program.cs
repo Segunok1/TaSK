d31f4de2f844eade026adaf914dd860e4a425d6d
Segunok1 <k077ma777@yandex.ru>
2022-09-04 21:19:54 +0300

    Решение задачи №52

--- task52/Program.cs
+++ task52/Program.cs
@@ -7,3 +7,54 @@
 */
 
 
+Console.Write("Задайте строки двумерного массива m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте столбцы двумерного массива n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+
+void TwoDimensionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(10);
+        }
+    }
+}
+
+void PrintSumArray(int[,] array)
+{
+    int[] sum = new int[n];
+    for (int i = 0; i <  array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sum[i] += array[j, i];
+        }
+    }
+    for (int i = 0; i <  array.GetLength(1); i++)
+    {
+        Console.Write((double)sum[i] /  array.GetLength(1) + " ");
+    }
+
+    Console.WriteLine();
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+TwoDimensionalArray(array);
+PrintArray(array);
+PrintSumArray(array);
