8024863f2c1c39c441fdb8597fda4f81acfc7d27
Segunok1 <k077ma777@yandex.ru>
2022-09-22 18:32:55 +0300

    Решение задачи №66

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,15 +2,15 @@
 Console.WriteLine(a);
 int b = 7;
 Console.WriteLine(b);
-if(a > b)
+if (a > b)
 {
-   int max = a;
-   Console.Write("max = ");
-   Console.WriteLine(max);
+    int max = a;
+    Console.Write("max = ");
+    Console.WriteLine(max);
 }
 else
 {
-    int max = b; 
+    int max = b;
     Console.Write("max = ");
     Console.WriteLine(max);
-} 
+}
---
+++ task66/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму
+натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+
+void PrintArray(int m, int n){
+    Console.Write(Sum(m, n));
+} 
+
+int Sum(int m, int n)
+{
+    if (m >= n)
+        return 1;
+    int result = Sum(m, n - 1);
+    return n + result;
+}
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Sum(m, n);
+PrintArray(m, n);
